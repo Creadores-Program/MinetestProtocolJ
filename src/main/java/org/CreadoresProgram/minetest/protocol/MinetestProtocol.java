529c7f58c791711df6c900ffcba23865a612060e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-10 23:13:01 -0600

    Add MinetestProtocol class for packet handling

---
+++ src/main/java/org/CreadoresProgram/minetest/protocol/MinetestProtocol.java
@@ -0,0 +1,10 @@
+package org.CreadoresProgram.minetest.protocol;
+
+import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.Server;
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.crypt.PacketEncryption;
+import com.github.steveice10.packetlib.packet.PacketHeader;
+import com.github.steveice10.packetlib.packet.PacketProtocol;
+
+public class MinetestProtocol extends PacketProtocol{}
