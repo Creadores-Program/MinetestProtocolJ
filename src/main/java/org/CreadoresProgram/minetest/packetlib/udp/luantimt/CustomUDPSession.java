e2b62635b5c3f99b64d7b2546f75a4f82235454b
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-11 13:17:20 -0600

    Create CustomUDPSession class for UDP packet handling

---
+++ src/main/java/org/CreadoresProgram/minetest/packetlib/udp/luantimt/CustomUDPSession.java
@@ -0,0 +1,6 @@
+package org.CreadoresProgram.minetest.packetlib.udp.luantimt;
+
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.packet.Packet;
+
+public abstract class CustomUDPSession /*extends SimpleChannelInboundHandler<Packet>*/ implements Session{}
