b967856907d43a5b856f36651438e738bf696f48
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-16 23:49:14 -0600

    Implement MinetestProtocol with client and server session handling

--- src/main/java/org/CreadoresProgram/minetest/protocol/MinetestProtocol.java
+++ src/main/java/org/CreadoresProgram/minetest/protocol/MinetestProtocol.java
@@ -6,5 +6,24 @@ import com.github.steveice10.packetlib.Session;
 import com.github.steveice10.packetlib.crypt.PacketEncryption;
 import com.github.steveice10.packetlib.packet.PacketHeader;
 import com.github.steveice10.packetlib.packet.PacketProtocol;
+import org.CreadoresProgram.minetest.protocol.packet.client.*;
 
-public class MinetestProtocol extends PacketProtocol{}
+public class MinetestProtocol extends PacketProtocol{
+  @SuppressWarnings("unused")
+	private MinetestProtocol() {
+  }
+  @Override
+  public String getSRVRecordPrefix() {
+      return "_minetest_luanti";
+  }
+  @Override
+	public void newClientSession(Client client, Session session) {
+    this.registerOutgoing(0x02, ClientInitPacket.class);
+    this.registerOutgoing(0x11, ClientInit2Packet.class);
+  }
+  @Override
+	public void newServerSession(Server server, Session session) {
+    this.registerIncoming(0x02, ClientInitPacket.class);
+    this.registerIncoming(0x11, ClientInit2Packet.class);
+  }
+}
