b6912428a47736b57ab9b3e144cc41221023b215
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-16 23:34:56 -0600

    Create MinetestConstants for protocol versioning
    Added MinetestConstants class with protocol version constants.

---
+++ src/main/java/org/CreadoresProgram/minetest/protocol/MinetestConstants.java
@@ -0,0 +1,10 @@
+package org.CreadoresProgram.minetest.protocol;
+/**
+ * Contains various protocol constants.
+ */
+public class MinetestConstants {
+  //supported network protocol range
+  public static final int MIN_PROTOCOL_VERSION = 37;
+  //max supported network protocol
+  public static final int MAX_PROTOCOL_VERSION = 52;
+}
