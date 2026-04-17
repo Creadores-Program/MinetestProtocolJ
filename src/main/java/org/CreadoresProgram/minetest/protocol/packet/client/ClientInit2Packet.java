78b3c5590e7c623bb2cffa7e0a02546f137c8414
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-04-16 23:26:15 -0600

    Rename ClientInitPacket constructor to ClientInit2Packet

---
+++ src/main/java/org/CreadoresProgram/minetest/protocol/packet/client/ClientInit2Packet.java
@@ -0,0 +1,18 @@
+package org.CreadoresProgram.minetest.protocol.packet.client;
+import com.github.steveice10.packetlib.io.NetInput;
+import com.github.steveice10.packetlib.io.NetOutput;
+import com.github.steveice10.packetlib.packet.Packet;
+
+import java.io.IOException;
+
+public class ClientInit2Packet implements Packet{
+
+  private ClientInit2Packet() {
+  }
+  @Override
+  public void read(NetInput in) throws IOException {
+  }
+  @Override
+  public void write(NetOutput out) throws IOException {
+  }
+}
